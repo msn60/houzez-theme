186af8bbb145c7a3ada08cf25a9c45ed30eacdfe
msn60 <mehdisoli333@hotmail.com>
2017-11-12 17:34:10 +0330

    first commit in begining of project

---
+++ houzez-child/functions.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+==================================
+Hooks
+==================================
+*/
+
+/*Enqueueing parent styles and child styles*/
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'parent-style'; 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+}
+
+
+
+/*Add custom JS file to run in houzez theme*/
+function msn_add_houzez_js() {
+
+    wp_enqueue_script('msn-houzez-custom.script', get_stylesheet_directory_uri().'/msn-houzez-1-0-0.js',array('jquery'),2,true);
+}
+
+add_action( 'wp_enqueue_scripts', 'msn_add_houzez_js', 100 );
+
+
+
+/*Add Language subdirectory in Child theme*/
+function msn_houzez_languages_theme_setup() {
+    load_child_theme_textdomain( 'houzez', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'msn_houzez_languages_theme_setup' );
+
+
+/*
+==================================
+Small functions
+==================================
+*/
+
+
+
+/*
+==================================
+Set up
+==================================
+*/
+
+
+/*
+==================================
+Includes 
+==================================
+*/
+
+
+/*
+==================================
+Shortcodes
+==================================
+*/
+
+
---
+++ houzez-child/msn-houzez-1-0-0.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $(document).ready(function () {
+        var $window = $(window);
+        $window.on('load',function () {
+            /*Only for test my js file*/
+            console.log('salam gholam');
+
+        });
+
+
+    });
+
+})(jQuery);
